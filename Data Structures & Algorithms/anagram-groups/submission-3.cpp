3f1219d0a6c43c6ef5fdb20a67bc8514fef56385
Nguyễn Gia Bảo <25520145@gm.uit.edu.vn>
2026-09-24 11:54:23 +0700

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map < string, vector<string>> map;
+        for ( string s: strs)
+        {
+            string key=s;
+            sort(key.begin(),key.end());
+            map[key].push_back(s);
+        }
+        vector<vector<string>> save;
+        for(auto& pair: map)
+        {save.push_back(pair.second);
+        }
+        return save;
+    }
+};
